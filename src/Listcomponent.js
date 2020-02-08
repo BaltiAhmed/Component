58f679e7484e9366ec25a1d5f3493c268015854e
GMC2020 <ahmedbalti06@gmail.com>
2020-02-08 16:28:25 +0100

    first commit

--- src/Listcomponent.js
+++ src/Listcomponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Photo from './Photo';
 import Title from './Title';
+import photo from './photo1.jpg';
 import './style.css';
 
 function Listcomponent() {
@@ -10,6 +12,7 @@ function Listcomponent() {
 
     <div className="Menu">
       <h2 className="desc">Menu</h2>
+      <Photo src="/photo1.png" />
       <Title style={{
 
         color: "red"
---
+++ src/Photo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Photo(props) {
+
+
+
+    return (
+
+        
+        <img src={props.src}/>
+
+
+
+
+
+       
+
+
+
+    );
+
+}
+
